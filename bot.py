8a3572bd845f661fa4916ab4e75e66d776c62eb2
Alexander Vanbiervliet <alexandervanbiervliet@gmail.com>
2023-11-27 17:28:08 +0300

    initial commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import logging
+import os
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from config import TOKEN
+from transcriptor import transcript
+import asyncio
+
+logging.basicConfig(level=logging.INFO)
+# TOKEN = os.getenv('TOKEN')
+bot = Bot(token = TOKEN)
+dp = Dispatcher()
+
+logging.basicConfig(level=logging.INFO)
+
+@dp.message(CommandStart())
+async def cmd_start(msg: types.Message):
+ 
+    await msg.answer(text = f'Enter your name is cyryllic letters:')
+
+
+@dp.message()
+async def message(msg: types.Message):
+    user_name = msg.from_user.first_name
+    user_msg = transcript(msg.text)
+    await msg.answer(text = f'Hello, {user_name}, your transcribed name is: {user_msg}')
+
+async def main() -> None:
+    bot = Bot(token = TOKEN)
+
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ transcriptor.py
@@ -0,0 +1,9 @@
+import re
+
+def transcript(name: str) ->str:
+    my_dict = {'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E', 'Ж': 'ZH', 'З': 'Z','И': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 'М':'M', 'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С':'S', 'Т': 'T', 'У':'U', 'Ф': 'F', 'Х': 'KH', 'Ц': 'TS', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SCH', 'Ы': 'Y','Ъ': 'IE','Ь':'', 'Э': 'E', 'Ю': 'IU', 'Я': 'IA'}
+    if re.match('^[А-Яа-яЁё ]+$', name):
+        return ''.join([my_dict.get(c, c) for c in name.upper()])
+    else:
+        return 'invalid input'
+
