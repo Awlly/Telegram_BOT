d01abf7a14cb074c45f73d5ae78fca78bd35542b
Alexander Vanbiervliet <alexandervanbiervliet@gmail.com>
2023-11-29 20:41:48 +0300

    final changes to bot, to prepare it for docker

--- bot.py
+++ bot.py
@@ -2,17 +2,15 @@ import logging
 import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-from config import TOKEN
+# from config import TOKEN
 from transcriptor import transcript
 import asyncio
 
 logging.basicConfig(filename='logs.txt', level=logging.DEBUG,
                     format=' %(asctime)s - %(levelname)s - %(message)s')
-# TOKEN = os.getenv('TOKEN')
+TOKEN = os.getenv('TOKEN')
 bot = Bot(token = TOKEN)
 dp = Dispatcher()
-
-logging.basicConfig(level=logging.INFO)
 
 @dp.message(CommandStart())
 async def cmd_start(msg: types.Message):
--- transcriptor.py
+++ transcriptor.py
@@ -1,14 +1,15 @@
-import re
 
-def transcript(name: str) ->str:
-    my_dict = {'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
-                'Ж': 'ZH', 'З': 'Z','И': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 'М':'M', 
-                'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С':'S', 'Т': 'T', 'У':'U', 
-                'Ф': 'F', 'Х': 'KH', 'Ц': 'TS', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SCH', 
-                'Ы': 'Y','Ъ': 'IE','Ь':'', 'Э': 'E', 'Ю': 'IU', 'Я': 'IA', ' ': ' '}
+transcription_dict = {'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Д': 'D', 'Е': 'E', 'Ё': 'E',
+            'Ж': 'ZH', 'З': 'Z','И': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 'М':'M', 
+            'Н': 'N', 'О': 'O', 'П': 'P', 'Р': 'R', 'С':'S', 'Т': 'T', 'У':'U', 
+            'Ф': 'F', 'Х': 'KH', 'Ц': 'TS', 'Ч': 'CH', 'Ш': 'SH', 'Щ': 'SCH', 
+            'Ы': 'Y','Ъ': 'IE','Ь':'', 'Э': 'E', 'Ю': 'IU', 'Я': 'IA', ' ': ' '}
+
+def transcript(name: str, my_dict = transcription_dict) ->str:
     
-    if all(i in my_dict for i in name):
+    if all(i in my_dict for i in name.upper()):
         return ''.join([my_dict.get(c, c) for c in name.upper()])
     else:
         return 'invalid input'
 
+print(transcript('вася пупкин'))
